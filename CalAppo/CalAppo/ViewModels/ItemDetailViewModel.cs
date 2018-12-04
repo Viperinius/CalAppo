8d8f73715e5baa0c931fad105c38ef0788791d46
Viperinius <41393831+Viperinius@users.noreply.github.com>
2018-12-04 23:07:03 +0100

    Projektdateien hinzufügen.

---
+++ CalAppo/CalAppo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CalAppo.Models;
+
+namespace CalAppo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
